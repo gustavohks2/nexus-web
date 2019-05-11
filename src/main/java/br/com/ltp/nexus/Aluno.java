0778d36059d5d2ea1cb42519cd20bc520d50c446
Gustavo <gustavohks2@gmail.com>
2019-05-11 16:50:54 -0300

    Cria configuração inicial de persistência

--- src/main/java/br/com/ltp/nexus/Aluno.java
+++ src/main/java/br/com/ltp/nexus/Aluno.java
@@ -1,5 +1,24 @@
 package br.com.ltp.nexus;
 
-public class Aluno {
-    
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
